1acf69e69e2fd335aedd0c2ee6618d28e5ade8d0
Imgyu Kim <kimimgo@gmail.com>
2018-07-27 14:49:29 +0900

    Update Form1.cs

--- CATIATranslator/Form1.cs
+++ CATIATranslator/Form1.cs
@@ -67,44 +67,17 @@ namespace CATIATranslator
         // Pre-Processor Assembly
         private void button3_Click_1(object sender, EventArgs e)
         {
-
-
-            //string filePath = CATPartOpenDialog();
-
-
-            /*
-           파일 경로
-           "D:\project\Macro\TestAssem\SubAssembly1\RotationPart.CATPart"
-           "D:\project\Macro\TestAssem\SubAssembly1\Body.CATPart"
-
-           coaxial - catCstTypeOn
-           "Product1/Part1.1/!Axis:(Selection_RSur:(Face:(Brp:(Pocket.1;0:(Brp:(Sketch.2;4)));None:();Cf11:());Pocket.1_ResultOUT;Z0;G3563))"
-           "Product1/Part1.2/!Axis:(Selection_RSur:(Face:(Brp:(Pad.2;0:(Brp:(Sketch.2;4)));None:();Cf11:());Pad.2_ResultOUT;Z0;G3563))"
-           incidence - catCstTypeSurfContact
-           "Product1/Part1.1/!Selection_RSur:(Face:(Brp:(Pad.1;1);None:();Cf11:());Pad.1_ResultOUT;Z0;G3563))"
-           "Product1/Part1.2/!Selection_RSur:(Face:(Brp:(Pad.1;2);None:();Cf11:());Pad.1_ResultOUT;Z0;G3563))"
-            */
-
-            // List<PreBuffer> stack = new List<PreBuffer>();
-            // stack.Add(new PreBuffer("D:\\project\\Macro\\TestAssem\\SubAssembly1\\RotationPart.CATPart","Part1","Rotation"));
-            // stack.Add(new PreBuffer("D:\\project\\Macro\\TestAssem\\SubAssembly1\\Body.CATPart","Part1","Body"));
             string path = Directory.GetCurrentDirectory();
             PreStack stack = new PreStack();
             stack.Clear();
-            //  stack.StackItem("D:\\project\\Macro\\TestAssem\\SubAssembly1\\RotationPart.CATPart", "Part1", "RotationPart");
-            //  stack.StackItem("D:\\project\\Macro\\TestAssem\\SubAssembly1\\Body.CATPart", "Part1", "Body");
             stack.StackItem(path + "\\TESTModel\\SubAssembly1\\RotationPart.CATPart", "Part1", "RotationPart");
             stack.StackItem(path + "\\TESTModel\\SubAssembly1\\Body.CATPart", "Part1", "Body");
 
             ReferenceClass.ref_Pre m_refer = new ReferenceClass.ref_Pre();
-            //m_refer.PreTest(stack,stack.GetSize(), "catCstTypeOn", "Product1/Part1.1/!Axis:(Selection_RSur:(Face:(Brp:(Pocket.1;0:(Brp:(Sketch.2;4)));None:();Cf11:());Pocket.1_ResultOUT;Z0;G3563))", "Product1/Part1.2/!Axis:(Selection_RSur:(Face:(Brp:(Pad.2;0:(Brp:(Sketch.2;4)));None:();Cf11:());Pad.2_ResultOUT;Z0;G3563))");
             m_refer.SetConstraint(stack, stack.GetSize(), "catCstTypeOn", "Product1/Part1.1/!Axis:(Selection_RSur:(Face:(Brp:(Pocket.1;0:(Brp:(Sketch.2;4)));None:();Cf11:());Pocket.1_ResultOUT;Z0;G3563))", "Product1/Part1.2/!Axis:(Selection_RSur:(Face:(Brp:(Pad.2;0:(Brp:(Sketch.2;4)));None:();Cf11:());Pad.2_ResultOUT;Z0;G3563))", "move", 0);
             m_refer.SetConstraint(stack, stack.GetSize(), "catCstTypeSurfContact", "Product1/Part1.1/!Selection_RSur:(Face:(Brp:(Pad.1;1);None:();Cf11:());Pad.1_ResultOUT;Z0;G3563))", "Product1/Part1.2/!Selection_RSur:(Face:(Brp:(Pad.1;2);None:();Cf11:());Pad.1_ResultOUT;Z0;G3563))", "", 0);
-            // m_refer.SetConstraint(stack, stack.GetSize(), "catCstTypeOn", "Product1/Part1.2/!Axis:(Selection_RSur:(Face:(Brp:(Pocket.1;0:(Brp:(Sketch.2;4)));None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162))", "Product1/Part1.1/!Axis:(Selection_RSur:(Face:(Brp:(Pocket.1;0:(Brp:(Sketch.2;4)));None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162))", "move", 0);
-            //m_refer.SetConstraint(stack, stack.GetSize(), "catCstTypeSurfContact", "Product1/Part1.2/!Selection_RSur:(Face:(Brp:(Pad.1;0:(Brp:(Sketch.1;8)));None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162)", "Product1/Part1.1/!Selection_RSur:(Face:(Brp:(Pad.1;0:(Brp:(Sketch.1;8)));None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162)", "same", 0);
-            //m_refer.SetConstraint(stack, stack.GetSize(), "catCstTypeSurfContact", "Product1/Part1.2/!Selection_RSur:(Face:(Brp:(Pad.1;0:(Brp:(Sketch.1;8)));None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162)", "Product1/Part1.1/!Selection_RSur:(Face:(Brp:(Pad.1;0:(Brp:(Sketch.1;8)));None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162)", "", 0);
-            //m_refer.SetConstraint(stack, stack.GetSize(), "catCstTypeSurfContact", "Product1/Part1.2/!Selection_RSur:(Face:(Brp:(Pad.1;2);None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162)", "Product1/Part1.1/!Selection_RSur:(Face:(Brp:(Pad.1;2);None:();Cf11:());EdgeFillet.1_ResultOUT;Z0;G4162)", "same", 0);
-
+            //To Hellen, Generalize 필요함.
+            //Post 쪽 참조.
         }
 
         // Post-Processor Assembly
